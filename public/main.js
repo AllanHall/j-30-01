8f681d5cc29d7cfc5646fb445a3d3a5bb89ec6d4
Allan Hall <bigal76126@gmail.com>
2019-07-28 20:50:48 -0400

    starting 30 days of vanilla js

--- public/main.js
+++ public/main.js
@@ -5,3 +5,12 @@ const main = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
+// eslint-disable-next-line space-before-function-paren
+document.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keycode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keycode}"]`)
+  if (!audio) return
+  audio.currentTime = 0
+  audio.play()
+  key.classList.add('playing')
+})
